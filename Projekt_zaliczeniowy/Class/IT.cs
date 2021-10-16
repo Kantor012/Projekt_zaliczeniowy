8ebc03aae7606f55b026b064615147f5aa021f8f
Kantor012 <47494655+Kantor012@users.noreply.github.com>
2021-10-16 15:23:21 +0200

    Add files via upload

---
+++ Projekt_zaliczeniowy/Class/IT.cs
@@ -0,0 +1,19 @@
+﻿namespace Projekt_zaliczeniowy.Class
+{
+    public class IT : Urzadzenia
+    {
+        public int ilosc_it { get; set; }
+        public int ram { get; set; }
+        public int przekatna_ekranu { get; set; }
+        public string cpu { get; set; }
+        public string gpu { get; set; }
+        public string model { get; set; }
+        public string marka { get; set; }
+        public IT()
+        {
+            System.Console.WriteLine("Jestem IT");
+        }
+
+       
+    }
+}
---
+++ Projekt_zaliczeniowy/Class/Odkurzacz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Projekt_zaliczeniowy.Class
+{
+    public class Odkurzacz : AGD
+    {
+        public int pojemnosc { get; set; }
+        public int zasieg_pracy { get; set; }
+        public Odkurzacz()
+        {
+            System.Console.WriteLine("Jestem konstruktorem");
+        }
+
+        public void dodaj()
+        {
+            ilosc++;
+            id++;
+            //rodzaj = System.Console.ReadLine();
+            System.Console.WriteLine("Producent:");
+            producent = System.Console.ReadLine();
+            System.Console.WriteLine("Klasa energetyczna:");
+            klasa_energetyczna = Convert.ToChar(Console.ReadLine());
+            System.Console.WriteLine("Moc [W]:");
+            moc = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Waga [kg]:");
+            waga = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Pojemność pojemnika/worka [L]:");
+            pojemnosc = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("zasieg pracy [m]:");
+            zasieg_pracy = Convert.ToInt32(Console.ReadLine());
+            rodzaj_urz.Add("Odkurzacz");
+        }
+    }
+}
